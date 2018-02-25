e0f52d036607840d344e73410c3a3ba1ff4b75ba
Asen Vasilev <a.vasilev_26@abv.bg>
2018-02-25 14:26:04 +0200

    fuck off

---
+++ eclipse-workspace/lecture_qac10_new/src/main/java/bg/pragmatic/lecture_qac10_new/App.java
@@ -0,0 +1,13 @@
+package bg.pragmatic.lecture_qac10_new;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
